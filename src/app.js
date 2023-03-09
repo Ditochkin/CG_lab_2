959054ab534f8fe48d4e91766a9740760e552494
Ditochkin <ditochkin@bk.ru>
2023-03-09 15:08:43 +0300

    Added line square

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ var gl; // глобальная переменная для контекста W
 const vsSource = 'attribute vec3 vertPosition;\n' +
                  'attribute vec3 vertColor;\n' +
                  'varying vec3 fragColor;\n' +
+                 'varying vec3 fragPosition;\n' +
                  'uniform mat4 mWorld;\n' +
                  'void main()\n' +
                  '{\n' +
                  '  fragColor = vertColor;\n' +
+                 '  fragPosition = vertPosition;\n' +
                  '  gl_Position = mWorld * vec4(vertPosition, 1.0);\n' +
                  '}'
 
@@ -17,7 +19,21 @@ const fsSource = 'precision mediump float;\n' +
                  '  gl_FragColor = vec4(fragColor, 1.0);\n' +
                  '}'
 
-
+const fsSourceLines = 'precision mediump float;\n' +
+                'varying vec3 fragColor;\n' +
+                'varying vec3 fragPosition;\n' +
+                'void main()\n' + 
+                '{\n' +
+                'int x = int(fragPosition.x * 10.0);\n' +
+                'float y = mod(float(x), 2.0);\n' + 
+                'if ((y == 0.0) && (x < 0) || (y != 0.0) && (x >= 0) || fragPosition.x <= 0.0 && fragPosition.x >= -0.1){\n' +
+                'gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n' +
+                '}\n' +
+                'else{' +
+                'gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);' +
+                '}\n' +
+                '}'
+                 
 function initWebGL(canvas) 
 {
     gl = null;
@@ -53,7 +69,7 @@ function loadShader(gl, type, source)
     return shader;
 }
 
-function initBuffersTriangle() 
+function initBuffersPolygon() 
 {
 	let triangleVerticesBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVerticesBuffer);
@@ -73,7 +89,7 @@ function initBuffersTriangle()
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
-function initBuffersSquare() 
+function initBuffersCube() 
 {
 	let squareVerticesBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
@@ -138,6 +154,22 @@ function initBuffersSquare()
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
+function initBuffersSquare()
+{
+    let squareVerticesBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+	let vertices = 
+    [ 
+        -0.5, -0.5, 0.0,     0.2, 0.3, 0.5,
+        -0.5, 0.5, 0.0,     0.2, 0.3, 0.5,
+        0.5, -0.5, 0.0,     0.2, 0.3, 0.5,
+        0.5, 0.5, 0.0,     0.2, 0.3, 0.5,
+        0.0, 0.0, 0.0,     0.2, 0.3, 0.5,
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
 
 function initShaderProgram(gl, vsSource, fsSource) 
 {
@@ -162,13 +194,13 @@ function initShaderProgram(gl, vsSource, fsSource)
 let vertexPositionAttribute
 let vertColorAttribute
 
-function drawScene() {
+function drawScenePolygon() {
 	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 0);
 	gl.vertexAttribPointer(vertColorAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 3*Float32Array.BYTES_PER_ELEMENT);
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 80);
 }
 
-function drawSceneQuad() {
+function drawSceneCube() {
 
 	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 0);
 	gl.vertexAttribPointer(vertColorAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 3*Float32Array.BYTES_PER_ELEMENT);
@@ -177,15 +209,27 @@ function drawSceneQuad() {
     gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
 	gl.drawArrays(gl.TRIANGLES, 0, 40);
+}
+
+function drawSceneLineQuad() {
+
+	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.vertexAttribPointer(vertColorAttribute, 3, gl.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 3*Float32Array.BYTES_PER_ELEMENT);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 40);
 
 }
 
 function start() 
 {
+    // ============== Polygon =============
     var canvas = document.getElementById("glcanvas");
     gl = initWebGL(canvas); // инициализация контекста GL – сами пишем
 
-    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    const shaderProgramPoltgon = initShaderProgram(gl, vsSource, fsSource);
     // продолжать только если WebGL доступен и работает  
 
     if (gl) 
@@ -202,23 +246,25 @@ function start()
         gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT); 
     }
 
-    initBuffersTriangle()
-    vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertPosition");
-    vertColorAttribute = gl.getAttribLocation(shaderProgram, "vertColor");
+    initBuffersPolygon()
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgramPoltgon, "vertPosition");
+    vertColorAttribute = gl.getAttribLocation(shaderProgramPoltgon, "vertColor");
     gl.enableVertexAttribArray(vertexPositionAttribute);
     gl.enableVertexAttribArray(vertColorAttribute);
-    gl.useProgram(shaderProgram);
+    gl.useProgram(shaderProgramPoltgon);
 
-    var matWorldUniformLocation = gl.getUniformLocation(shaderProgram, 'mWorld');
+    var matWorldUniformLocation = gl.getUniformLocation(shaderProgramPoltgon, 'mWorld');
 
 	var worldMatrix = new Float32Array(16);
 	glMatrix.mat4.identity(worldMatrix);
 
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-    drawScene()
+    drawScenePolygon()
 
-    var canvas2 = document.getElementById("squareCanvas");
+    // ============== Cube =============
+
+    var canvas2 = document.getElementById("cubeCanvas");
     initWebGL(canvas2)
     if (gl) 
     { // продолжать только если WebGL доступен и работает
@@ -234,16 +280,16 @@ function start()
         gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT); 
     }
 
-    const shaderProgramSquare = initShaderProgram(gl, vsSource, fsSource);
+    const shaderProgramCube = initShaderProgram(gl, vsSource, fsSource);
 
-    initBuffersSquare()
-    vertexPositionAttribute = gl.getAttribLocation(shaderProgramSquare, "vertPosition");
-    vertColorAttribute = gl.getAttribLocation(shaderProgramSquare, "vertColor");
+    initBuffersCube()
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgramCube, "vertPosition");
+    vertColorAttribute = gl.getAttribLocation(shaderProgramCube, "vertColor");
     gl.enableVertexAttribArray(vertexPositionAttribute);
     gl.enableVertexAttribArray(vertColorAttribute);
-    gl.useProgram(shaderProgramSquare);
+    gl.useProgram(shaderProgramCube);
 
-    matWorldUniformLocation = gl.getUniformLocation(shaderProgramSquare, 'mWorld');
+    matWorldUniformLocation = gl.getUniformLocation(shaderProgramCube, 'mWorld');
 
 	worldMatrix = new Float32Array(16);
 	glMatrix.mat4.identity(worldMatrix);
@@ -261,7 +307,46 @@ function start()
     glMatrix.mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-    drawSceneQuad()
+    drawSceneCube()
+
+    // ============== Square =============
+
+    var canvas3 = document.getElementById("lineSquareCanvas");
+    initWebGL(canvas3); // инициализация контекста GL – сами пишем
+
+    const shaderProgramLineSquare = initShaderProgram(gl, vsSource, fsSourceLines);
+    // продолжать только если WebGL доступен и работает  
+
+    if (gl) 
+    { // продолжать только если WebGL доступен и работает
+        // Устанавливаем размер вьюпорта
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        // установить в качестве цвета очистки буфера цвета черный, полная непрозрачность
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        // включает использование буфера глубины
+        gl.enable(gl.DEPTH_TEST);
+        // определяет работу буфера глубины: более ближние объекты перекрывают дальние
+        gl.depthFunc(gl.LEQUAL);
+        // очистить буфер цвета и буфер глубины
+        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT); 
+    }
+
+    initBuffersSquare()
+
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgramLineSquare, "vertPosition");
+    vertColorAttribute = gl.getAttribLocation(shaderProgramLineSquare, "vertColor");
+    gl.enableVertexAttribArray(vertexPositionAttribute);
+    gl.enableVertexAttribArray(vertColorAttribute);
+    gl.useProgram(shaderProgramLineSquare);
+
+    var matWorldUniformLocation = gl.getUniformLocation(shaderProgramLineSquare, 'mWorld');
+
+	var worldMatrix = new Float32Array(16);
+	glMatrix.mat4.identity(worldMatrix);
+
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+    drawSceneLineQuad()
 }
 
 start() 
